91b97a80c15d0fa9be2a6eb65d4625d41289667a
odde90 <43670486+odde90@users.noreply.github.com>
2020-05-27 20:55:43 +0200

    tested material ui

--- src/Componenets/App.tsx
+++ src/Componenets/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css'
+import { Button } from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
-
+      <button> test button with material ui</button>
     </div>
   );
 }
