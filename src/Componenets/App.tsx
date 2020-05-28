5fd797f5f3e4b2f86f56d10791384edf2a435516
odde90 <43670486+odde90@users.noreply.github.com>
2020-05-28 16:11:10 +0200

    added navbar and some buttons

--- src/Componenets/App.tsx
+++ src/Componenets/App.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css'
-import { Button } from '@material-ui/core'
+import Frontpage from './frontpage'
+import NavbarScroller from './navbar';
+import { Button } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <button> test button with material ui</button>
-    </div>
-  );
+const navigation = {
+  brand: { name: 'NavScroller', to: '/' },
+  links: [
+    { name: 'Item 1', to: '/' },
+    { name: 'Item 2', to: '/' },
+    { name: 'Item 3', to: '/' },
+    { name: 'Item 4', to: '/' },
+    { name: 'Item 5', to: '/' },
+    { name: 'Item 6', to: '/' },
+
+  ]
+};
+
+export default class App extends Component {
+  public render() {
+    const { brand, links } = navigation;
+
+    return (
+      <div className="App">
+        <NavbarScroller brand={brand} links={links} />
+        <div className="hello">
+          <ol>
+            <li>product 1</li>
+            <li>product 2</li>
+            <li>product 3</li>
+          </ol>
+
+          <Frontpage />
+          <Button variant="contained" color="secondary">
+            Secondary
+      </Button>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/Componenets/frontpage.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+interface State {
+
+}
+
+interface Props {
+
+}
+
+export default class Frontpage extends React.Component<Props, State>{
+    render() {
+        return (
+            <div>Hello</div>
+        );
+    }
+}
